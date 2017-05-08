40b67557e0406ca4db0589c7b4f36d0c9172f70e
Mara Sarabia <mara@Maras-MacBook-Pro.local>
2017-05-08 12:46:08 -0500

    adding initial rethinkdb example

---
+++ rethinkdb-example/rethinkdb_example.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+import os
+from urllib.parse import urlparse
+import json
+
+import rethinkdb as r
+
+app = Flask(__name__)
+
+# connection string and initialization
+RETHINKDB_URL = os.environ['RETHINKDB_URL']
+PATH_TO_CERT = os.environ['PATH_TO_CERT']
+parsed = urlparse(RETHINKDB_URL)
+conn = r.connect(
+    host=parsed.hostname,
+    port=parsed.port,
+    user=parsed.username,
+    password=parsed.password,
+    ssl={'ca_certs': PATH_TO_CERT})
+
+
+@app.route('/')
+# top-level page display
+def serve_page(name=None):
+    return render_template('index.html', name=name)
+
+@app.route('/words', methods=['PUT'])
+# triggers on hitting the 'Add' button; inserts word/definition into collection
+def handle_words(name=None):
+    new_word = {"word":request.form['word'], "definition":request.form['definition']}
+    conn.use("grand_tour")
+    r.table("python").insert(new_word).run(conn)
+    return "ECHO: PUT\n"
+
+@app.route('/words', methods=['GET'])
+# query for all the words in the table, returns as json for display on the page.
+def display_find(name=None):
+    conn.use("grand_tour")
+    cursor_obj = r.table('python').pluck("word", "definition").run(conn)
+    word_list = list(cursor_obj)
+    return json.dumps(word_list)
+
+if __name__ == "__main__":
+    app.run()
