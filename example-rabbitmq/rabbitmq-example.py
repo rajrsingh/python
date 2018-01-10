e10d22210baf77b700a7cf85ed390eb278cf8c5b
DailyAlice <mara.catherine@gmail.com>
2018-01-10 13:03:25 -0600

    updated for formatting and comments

--- example-rabbitmq/rabbitmq-example.py
+++ example-rabbitmq/rabbitmq-example.py
@@ -36,25 +36,30 @@ def serve_page():
 @app.route('/message', methods=['PUT'])
 # triggers on hitting the 'send' button; sends a message to the queue
 def send_message():
+    # makes a message from the field in the page
     msg = request.form['message']
-
+    # establishes a connection
     channel = connection.channel()
+    # selects or creates the message queue
     channel.queue_declare(queue='grand_tour')
+    # pushes the message onto the queue
     channel.basic_publish(
         exchange='',
         routing_key='grand_tour',
         body=msg)
-
     return msg
 
 
 @app.route('/message', methods=['GET'])
-# triggers on hitting the 'receive' button; retrieves a message to the queue
+# triggers on hitting the 'receive' button; retrieves a message from the queue
 def receive_message():
+    # establishes a connection
     channel = connection.channel()
+    # selects or creates the message queue
     channel.queue_declare(queue='grand_tour')
-
+    # retrieves a message from the queue, stores its parts
     method_frame, properties, body = channel.basic_get(queue = 'grand_tour', no_ack=True)
+    # returns message body or 'no message' for display on page
     if method_frame == None:
         return "{--No Messages in Queue--}"
     else:
