3752ca4df005ad3d4805c45b03a5ce5029860aa0
DailyAlice <mara.catherine@gmail.com>
2018-01-12 15:16:37 -0600

    adding ssl and comments and PUT return

--- example-mysql/mysql-example.py
+++ example-mysql/mysql-example.py
@@ -6,26 +6,27 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-import pymysql.cursors
+import mysql.connector
 
 
 app = Flask(__name__)
 
 # connection string and initialization - sets cursor class to handle python dicts
 compose_mysql_url = os.environ['COMPOSE_MYSQL_URL']
+path_to_mysql_cert = os.environ['PATH_TO_MYSQL_CERT']
 parsed = urlparse(compose_mysql_url)
 
-conn = pymysql.connect(
+conn = mysql.connector.connect(
     host=parsed.hostname,
     port=parsed.port,
     user=parsed.username,
     password=parsed.password,
-    db='grand_tour',
-    cursorclass=pymysql.cursors.DictCursor)
+    ssl_ca=path_to_mysql_cert,
+    database='grand_tour')
 
 
 @app.route('/')
-# top-level page display, creates mysql table on first run
+# top-level page display, creates table on first run
 def serve_page():
     cur = conn.cursor()
     cur.execute("""CREATE TABLE IF NOT EXISTS words (
@@ -42,16 +43,27 @@ def handle_words():
     cur.execute("""INSERT INTO words (word, definition)
         VALUES (%s, %s)""", (request.form['word'], request.form['definition']))
     conn.commit()
-    return "ECHO: PUT\n"
+    return ('', 204)
 
 
 @app.route('/words', methods=['GET'])
 # query for all the words in the table, returns as json for display on the page.
 def display_find():
     cur = conn.cursor()
+    
+    # SQL query for all the rows in the table, stores rows in an object
     cur.execute("""SELECT word, definition FROM words""")
-    results_list = cur.fetchall()
+    cursor_obj = cur.fetchall()
 
+    # grabs column names from the table
+    labels = [column[0] for column in cur.description]
+    
+    # makes a list from the dict of the zip of column names and the results object
+    results_list = []
+    for row in cursor_obj:
+        results_list.append(dict(zip(labels, row)))
+    
+    # makes json from the list for display on the page
     return json.dumps(results_list)
 
 
